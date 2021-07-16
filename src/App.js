7a2a18e3d62d701dd43821b59c72976aa76effca
Israel Leon <israelpat42@gmail.com>
2021-07-16 16:04:01 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GIT Commands
+          Learn GIT Commands conflicts
         </a>
+        <div> changes</div>
       </header>
     </div>
   );
