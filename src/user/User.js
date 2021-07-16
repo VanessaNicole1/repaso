02bb893c0addc74d7701a525e4b50d4e834085ca
Vanessa <vanessainiguez98@gmail.com>
2021-07-16 15:59:39 -0500

    Create user folder

---
+++ src/user/User.js
@@ -0,0 +1,13 @@
+
+
+function UserComponent() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>User</h1>
+      </header>
+    </div>
+  );
+}
+
+export default UserComponent;
